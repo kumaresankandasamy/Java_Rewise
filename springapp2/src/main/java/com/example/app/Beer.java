8c44674a8d157ba8cbf83710ed64dd6b5e9ed31d
unknown <kumaresank2000@gmail.com>
2019-12-14 11:08:08 +0530

    Creating a Rest API Effortlessly with Spring Rest Repositories

---
+++ springapp2/src/main/java/com/example/app/Beer.java
@@ -0,0 +1,41 @@
+package com.example.app;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private  double abv;
+
+    public Beer(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAbv() {
+        return abv;
+    }
+
+    public void setAbv(double abv) {
+        this.abv = abv;
+    }
+}
